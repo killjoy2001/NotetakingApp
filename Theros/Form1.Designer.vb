75b3c7ce3c526d632c7bc5d758c4330f919886d7
Kenny Aragon <kenny.ara.cas.ka@gmail.com>
2024-10-07 08:30:53 -0600

    v.1.0.1

---
+++ Theros/Form1.Designer.vb
@@ -0,0 +1,204 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        MenuStrip1 = New MenuStrip()
+        FontToolStripMenuItem = New ToolStripMenuItem()
+        ToolStripMenuItem2 = New ToolStripMenuItem()
+        ToolStripMenuItem3 = New ToolStripMenuItem()
+        ToolStripMenuItem4 = New ToolStripMenuItem()
+        richTextBox1 = New RichTextBox()
+        TabControl1 = New TabControl()
+        TabPage1 = New TabPage()
+        TextBox1 = New TextBox()
+        TabPage2 = New TabPage()
+        TextBox2 = New TextBox()
+        TabPage3 = New TabPage()
+        TextBox3 = New TextBox()
+        MenuStrip1.SuspendLayout()
+        TabControl1.SuspendLayout()
+        TabPage1.SuspendLayout()
+        TabPage2.SuspendLayout()
+        TabPage3.SuspendLayout()
+        SuspendLayout()
+        ' 
+        ' MenuStrip1
+        ' 
+        MenuStrip1.Items.AddRange(New ToolStripItem() {FontToolStripMenuItem})
+        MenuStrip1.Location = New Point(0, 0)
+        MenuStrip1.Name = "MenuStrip1"
+        MenuStrip1.Size = New Size(621, 24)
+        MenuStrip1.TabIndex = 4
+        MenuStrip1.Text = "MenuStrip1"
+        ' 
+        ' FontToolStripMenuItem
+        ' 
+        FontToolStripMenuItem.DropDownItems.AddRange(New ToolStripItem() {ToolStripMenuItem2, ToolStripMenuItem3, ToolStripMenuItem4})
+        FontToolStripMenuItem.Name = "FontToolStripMenuItem"
+        FontToolStripMenuItem.Size = New Size(66, 20)
+        FontToolStripMenuItem.Text = "Font Size"
+        ' 
+        ' ToolStripMenuItem2
+        ' 
+        ToolStripMenuItem2.Name = "ToolStripMenuItem2"
+        ToolStripMenuItem2.Size = New Size(86, 22)
+        ToolStripMenuItem2.Text = "16"
+        ' 
+        ' ToolStripMenuItem3
+        ' 
+        ToolStripMenuItem3.Name = "ToolStripMenuItem3"
+        ToolStripMenuItem3.Size = New Size(86, 22)
+        ToolStripMenuItem3.Text = "14"
+        ' 
+        ' ToolStripMenuItem4
+        ' 
+        ToolStripMenuItem4.Name = "ToolStripMenuItem4"
+        ToolStripMenuItem4.Size = New Size(86, 22)
+        ToolStripMenuItem4.Text = "12"
+        ' 
+        ' richTextBox1
+        ' 
+        richTextBox1.BorderStyle = BorderStyle.None
+        richTextBox1.Font = New Font("Segoe UI", 11.25F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        richTextBox1.Location = New Point(12, 37)
+        richTextBox1.Name = "richTextBox1"
+        richTextBox1.Size = New Size(407, 624)
+        richTextBox1.TabIndex = 9
+        richTextBox1.Text = ""
+        ' 
+        ' TabControl1
+        ' 
+        TabControl1.Controls.Add(TabPage1)
+        TabControl1.Controls.Add(TabPage2)
+        TabControl1.Controls.Add(TabPage3)
+        TabControl1.Location = New Point(425, 37)
+        TabControl1.Name = "TabControl1"
+        TabControl1.SelectedIndex = 0
+        TabControl1.Size = New Size(188, 624)
+        TabControl1.TabIndex = 10
+        ' 
+        ' TabPage1
+        ' 
+        TabPage1.Controls.Add(TextBox1)
+        TabPage1.Location = New Point(4, 24)
+        TabPage1.Name = "TabPage1"
+        TabPage1.Padding = New Padding(3)
+        TabPage1.Size = New Size(180, 596)
+        TabPage1.TabIndex = 0
+        TabPage1.Text = "Medical"
+        TabPage1.UseVisualStyleBackColor = True
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.BorderStyle = BorderStyle.None
+        TextBox1.Dock = DockStyle.Fill
+        TextBox1.Location = New Point(3, 3)
+        TextBox1.Multiline = True
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(174, 590)
+        TextBox1.TabIndex = 0
+        ' 
+        ' TabPage2
+        ' 
+        TabPage2.Controls.Add(TextBox2)
+        TabPage2.Location = New Point(4, 24)
+        TabPage2.Name = "TabPage2"
+        TabPage2.Padding = New Padding(3)
+        TabPage2.Size = New Size(180, 596)
+        TabPage2.TabIndex = 1
+        TabPage2.Text = "Insurance"
+        TabPage2.UseVisualStyleBackColor = True
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.BorderStyle = BorderStyle.None
+        TextBox2.Dock = DockStyle.Fill
+        TextBox2.Location = New Point(3, 3)
+        TextBox2.Multiline = True
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(174, 590)
+        TextBox2.TabIndex = 1
+        ' 
+        ' TabPage3
+        ' 
+        TabPage3.Controls.Add(TextBox3)
+        TabPage3.Location = New Point(4, 24)
+        TabPage3.Name = "TabPage3"
+        TabPage3.Size = New Size(180, 596)
+        TabPage3.TabIndex = 2
+        TabPage3.Text = "Financial"
+        TabPage3.UseVisualStyleBackColor = True
+        ' 
+        ' TextBox3
+        ' 
+        TextBox3.BorderStyle = BorderStyle.None
+        TextBox3.Dock = DockStyle.Fill
+        TextBox3.Location = New Point(0, 0)
+        TextBox3.Multiline = True
+        TextBox3.Name = "TextBox3"
+        TextBox3.Size = New Size(180, 596)
+        TextBox3.TabIndex = 1
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = Color.FromArgb(CByte(25), CByte(109), CByte(186))
+        ClientSize = New Size(621, 673)
+        Controls.Add(TabControl1)
+        Controls.Add(richTextBox1)
+        Controls.Add(MenuStrip1)
+        FormBorderStyle = FormBorderStyle.FixedSingle
+        Icon = CType(resources.GetObject("$this.Icon"), Icon)
+        MainMenuStrip = MenuStrip1
+        MaximizeBox = False
+        Name = "Form1"
+        Text = "Theros"
+        MenuStrip1.ResumeLayout(False)
+        MenuStrip1.PerformLayout()
+        TabControl1.ResumeLayout(False)
+        TabPage1.ResumeLayout(False)
+        TabPage1.PerformLayout()
+        TabPage2.ResumeLayout(False)
+        TabPage2.PerformLayout()
+        TabPage3.ResumeLayout(False)
+        TabPage3.PerformLayout()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents FontToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ToolStripMenuItem2 As ToolStripMenuItem
+    Friend WithEvents ToolStripMenuItem3 As ToolStripMenuItem
+    Friend WithEvents ToolStripMenuItem4 As ToolStripMenuItem
+    Friend WithEvents richTextBox1 As RichTextBox
+    Friend WithEvents TabControl1 As TabControl
+    Friend WithEvents TabPage1 As TabPage
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TabPage2 As TabPage
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TabPage3 As TabPage
+    Friend WithEvents TextBox3 As TextBox
+
+End Class
---
+++ Theros/Form1.vb
@@ -0,0 +1,171 @@
+﻿Imports System.IO
+Imports System.Collections.Generic
+Imports System.Text.RegularExpressions
+Public Class Form1
+
+    Private Sub richTextBox1_TextChanged(sender As Object, e As EventArgs) Handles richTextBox1.TextChanged
+
+        Dim cursorPosition As Integer = richTextBox1.SelectionStart
+        ' 
+        RemoveHandler richTextBox1.TextChanged, AddressOf richTextBox1_TextChanged
+        ' 
+        HighlightNumbers()
+        AbbReplacement()
+
+        richTextBox1.SelectionStart = cursorPosition
+        AddHandler richTextBox1.TextChanged, AddressOf richTextBox1_TextChanged
+    End Sub
+
+    ' *********************
+    ' NUMBERS *************
+
+    Private Sub HighlightNumbers() ' identifies numbers
+        Dim pattern As String = "\d+"
+        Dim matches As MatchCollection = Regex.Matches(richTextBox1.Text, pattern)
+        Dim originalStart As Integer = richTextBox1.SelectionStart
+        Dim originalLength As Integer = richTextBox1.SelectionLength
+
+        richTextBox1.SelectAll()
+        richTextBox1.SelectionBackColor = richTextBox1.BackColor
+
+        ' highlights numbers
+        For Each match As Match In matches
+            richTextBox1.Select(match.Index, match.Length)
+            richTextBox1.SelectionBackColor = Color.GreenYellow ' <--- u can change color here
+        Next
+        richTextBox1.Select(originalStart, originalLength)
+    End Sub
+
+    ' ***********************
+    ' FONT SIZE *************
+
+    ' changes font sizes according to menustrip selection
+    Private Sub ToolStripMenuItem2_Click(sender As Object, e As EventArgs) Handles ToolStripMenuItem2.Click
+        richTextBox1.Font = New Font(richTextBox1.Font.FontFamily, 16) ' <- font 16
+    End Sub
+
+    Private Sub ToolStripMenuItem3_Click(sender As Object, e As EventArgs) Handles ToolStripMenuItem3.Click
+        richTextBox1.Font = New Font(richTextBox1.Font.FontFamily, 14) ' <- font 14
+    End Sub
+
+    Private Sub ToolStripMenuItem4_Click(sender As Object, e As EventArgs) Handles ToolStripMenuItem4.Click
+        richTextBox1.Font = New Font(richTextBox1.Font.FontFamily, 12) ' <- font 12
+    End Sub
+
+    ' *********************
+    ' VOCAB TABS **********
+
+    ' saves vocab on txt and loads it when running theros
+    Private Sub Form1_FormClosing(sender As Object, e As FormClosingEventArgs) Handles MyBase.FormClosing
+        ' saves on txt
+        System.IO.File.WriteAllText("vocab1.txt", TextBox1.Text)
+        System.IO.File.WriteAllText("vocab2.txt", TextBox2.Text)
+        System.IO.File.WriteAllText("vocab3.txt", TextBox3.Text)
+    End Sub
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        AbbCheck()
+
+        If System.IO.File.Exists("vocab1.txt") Then
+            TextBox1.Text = System.IO.File.ReadAllText("vocab1.txt") ' loads vocab on TextBox1
+        End If
+        If System.IO.File.Exists("vocab2.txt") Then
+            TextBox2.Text = System.IO.File.ReadAllText("vocab2.txt") ' loads vocab on TextBox2
+        End If
+        If System.IO.File.Exists("vocab3.txt") Then
+            TextBox3.Text = System.IO.File.ReadAllText("vocab3.txt") ' loads vocab on TextBox3
+        End If
+    End Sub
+
+    ' ************************
+    ' abbreviations **********
+
+    ' path of txt file
+    Dim filePath As String = Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.Desktop), "abbreviations.txt")
+
+    ' dictionary
+    Private abbreviations As New Dictionary(Of String, String)
+    Private Sub AbbCheck()
+        If File.Exists(filePath) Then
+            Using reader As New StreamReader(filePath)
+                Dim line As String
+                abbreviations.Clear() ' cleans dic b4 loading
+
+                While True
+                    line = reader.ReadLine()
+                    If line Is Nothing Then Exit While ' exits loop if nothing
+
+                    Dim parts() As String = line.Split("="c)
+                    If parts.Length = 2 Then
+                        abbreviations(parts(0).Trim()) = parts(1).Trim()
+                    End If
+                End While
+            End Using
+        Else
+            MessageBox.Show("File abbreviations.txt is not available, please make sure it is created on desktop", "File not found", MessageBoxButtons.OK, MessageBoxIcon.Warning)
+        End If  'instead of just of displaying an error message, ill modofy this to create the txt doc automcly ***********************
+    End Sub
+    Private Sub AbbReplacement()
+        Dim originalStart As Integer = richTextBox1.SelectionStart
+        Dim originalText As String = richTextBox1.Text
+        Dim modifiedText As String = originalText
+        Dim abbReplaced As Boolean = False
+
+        RemoveHandler richTextBox1.TextChanged, AddressOf richTextBox1_TextChanged
+
+        ' replaces abbs
+        For Each abb In abbreviations
+            Dim abbKey As String = abb.Key
+            Dim pattern As String = String.Format("(?<=^| )({0})(?= |$)", Regex.Escape(abbKey))
+
+            If Regex.IsMatch(originalText, pattern) Then
+                modifiedText = Regex.Replace(modifiedText, pattern, abb.Value)
+                abbReplaced = True
+            End If
+        Next
+
+        If abbReplaced Then
+            richTextBox1.Text = modifiedText
+
+            ' moves cursor to the last caracter after replacing a word to avoid typing in the middle of a word
+            ' the only way this happends is by typing right ->, thats why SendKeys.Send("{RIGHT}")
+            Dim newCursorPosition As Integer = originalStart + (modifiedText.Length - originalText.Length)
+            richTextBox1.SelectionStart = newCursorPosition
+
+            ' 
+            SendKeys.Send("{RIGHT}")
+
+
+            HighlightReplacedWords()
+        End If
+
+        AddHandler richTextBox1.TextChanged, AddressOf richTextBox1_TextChanged
+    End Sub
+
+    ' this method is supposed to highlight replaced words, currently not working
+    Private Sub HighlightReplacedWords()
+        Dim texto As String = richTextBox1.Text
+        Dim originalStart As Integer = richTextBox1.SelectionStart
+        Dim lastReplacementIndex As Integer = -1
+
+        richTextBox1.SelectAll()
+        richTextBox1.SelectionBackColor = richTextBox1.BackColor
+
+        For Each abb In abbreviations
+            Dim startIndex As Integer = 0
+            Dim key As String = abb.Key
+            Dim value As String = abb.Value
+
+            While startIndex < texto.Length
+                startIndex = texto.IndexOf(value, startIndex)
+                If startIndex = -1 Then Exit While
+
+                richTextBox1.Select(startIndex, value.Length)
+                richTextBox1.SelectionBackColor = Color.Yellow
+                lastReplacementIndex = startIndex + value.Length
+                startIndex += value.Length
+            End While
+        Next
+    End Sub
+
+End Class
